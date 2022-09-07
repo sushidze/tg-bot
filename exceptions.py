0b729cc551c545a990bb1cab4459c8e2a63b49c7
Даниил Сухочев <daniilsuhocev@Daniils-MacBook-Pro.local>
2022-09-07 13:52:47 +0300

    Fixes after review

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,28 @@
 class ErrorMessage(Exception):
     """Исключение возникает из-за ошибок при отправке сообщения в чат."""
     pass
+
+
+class AnswerNot200(Exception):
+    """Исключение возникает из-за ошибки при запросе URL."""
+    pass
+
+
+class Homeworksnotlist(Exception):
+    """Исключение возникает из-за того, что homeworks не список."""
+    pass
+
+
+class AnswerNotCorrect(Exception):
+    """Исключение возникает из-за того, что ответ API не словарь."""
+    pass
+
+
+class StatusIsNotCorrect(Exception):
+    """Исключение возникает из-за того, что значения status нет в утверждённых."""
+    pass
+
+
+class NotAllTokens(Exception):
+    """Исключение возникает из-за того, что заданы не все токены."""
+    pass
--- homework.py
+++ homework.py
@@ -26,7 +26,7 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 RETRY_TIME = 6
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/77'
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
@@ -39,15 +39,7 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message):
     """Отправляем сообщение в чат."""
-    try:
-        bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.info(
-            f'В чат {TELEGRAM_CHAT_ID} отправлено сообщение {message}.'
-        )
-    except exceptions.ErrorMessage:
-        logging.error(
-            f'Ошибка при отправке сообщения в чат {TELEGRAM_CHAT_ID}.'
-        )
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
@@ -60,63 +52,61 @@ def get_api_answer(current_timestamp):
         params=params
     )
     if response.status_code != 200:
-        raise requests.ConnectionError(response.status_code, params)
+        logger.error(f'{params} Эндпоинт {ENDPOINT} недоступен.')
+        raise exceptions.AnswerNot200(response.status_code, params)
     return response.json()
 
 
 def check_response(response):
     """Проверка ответа API на корректность."""
-    if not isinstance(response, dict):
-        logger.error('Ответ API не словарь.')
-        raise TypeError('Ответ API не словарь.')
-    homeworks = response['homeworks']
-    if not isinstance(homeworks, list):
-        logger.error('Не получили список домашних работ.')
-        raise KeyError('Не получили список домашних работ.')
-    return homeworks
+    if not isinstance(response['homeworks'], list):
+        logger.error('Некорректный ответ сервера.')
+        raise exceptions.AnswerNotCorrect('Некорректный ответ сервера')
+    return response['homeworks']
 
 
 def parse_status(homework):
     """Получение информации о конкретной домашней работе."""
-    try:
-        homework_name = homework['homework_name']
-    except KeyError:
-        logging.error('Неверный ответ сервера')
+    homework_name = homework['homework_name']
     homework_status = homework['status']
     verdict = HOMEWORK_STATUSES[homework_status]
     if homework_status not in HOMEWORK_STATUSES:
-        message = 'Недокументированный статус домашней работы'
-        raise KeyError(message)
+        logger.error('Недокументированный статус домашней работы')
+        raise exceptions.StatusIsNotCorrect
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверяем доступность переменных окружения."""
-    return_value = all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
-    if not return_value:
-        logging.critical('Введены не все токены.')
-    return return_value
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
+
         exit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time() - 12000000)
     cash = ''
     while True:
         try:
             response = get_api_answer(current_timestamp)
             current_timestamp = response['current_date']
-            homework = check_response(response)[0]
-            message = parse_status(homework)
-            if cash != message:
-                cash = message
+            homeworks = check_response(response)
+            homework = homeworks[0]
+            homework_status = homeworks[0].get('status')
+            if cash != homework_status:
+                cash = homework_status
+                message = parse_status(homework)
                 send_message(bot, message)
-                logging.info('Сообщение о статусе ДЗ отправлено в чат')
-        except requests.ConnectionError as error:
-            logging.error(f'{error} Эндпоинт {ENDPOINT} недоступен.')
+                logger.info('Сообщение о статусе ДЗ отправлено в чат')
+        except exceptions.NotAllTokens:
+            logger.critical('Введены не все токены.')
+        except exceptions.ErrorMessage:
+            logger.error(
+                f'Ошибка при отправке сообщения в чат {TELEGRAM_CHAT_ID}.'
+            )
         finally:
             time.sleep(RETRY_TIME)
 
